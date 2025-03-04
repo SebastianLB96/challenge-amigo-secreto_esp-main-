d7c78c5d4fb74d5b2b35a2c802068a6d41afbea9
SebastianLB96 <sebas.lop96@gmail.com>
2025-03-03 23:46:49 -0500

    Actualización de comentarios.

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+//Limpia el input amigo
 function limpiar(){
     document.getElementById('amigo').value = '';
 }
 
+//Asigna texto al elemento enviado por parametro
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
 
+//Asigna texto inicial al elemento h2
 function parametroInciales(){
     asignarTextoElemento('h2','Digite el nombre de sus amigos');
 }
 
+//Inserta la persona en el array
 function insertarPersona(nuevaPersona){
     //Insertar nueva persona
     amigos.push(nuevaPersona);
 }
+
+//Realiza la validación de los datos ingresados para luego ser registrado o descartado.
 function agregarAmigo(){
     //Recuperar persona ingresada
     let personaIngresada = document.getElementById('amigo').value;
@@ -42,11 +48,13 @@ function agregarAmigo(){
     actualizarAmigos();   
 }
 
+//actualiza la lista cada vez que se inserta un nuevo amigo o se sortea.
 function actualizarAmigos(){
     let elementoHTML = document.getElementById('listaAmigos');
     elementoHTML.innerHTML = `<li> ${amigos}</li>` ;
 }
 
+//Realiza el sorteo del amigo secreto y al mismo tiempo elimina del array el amigo que ya fue sorteado.
 function sortearAmigo(){
     let amigoSecreto  = amigos[Math.floor(Math.random()*amigos.length)];
     let amigoGenerado = amigos.indexOf(amigoSecreto);
